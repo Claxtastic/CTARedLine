45bda9b0ec73b67d84d69f015ded803596f62aad
claxtastic <claxtons4@gmail.com>
2019-07-08 16:53:17 -0500

    Everything is mostly ready, just need to actually retweet matching tweets

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import tweepy
 import sqlite3
+import time
 
 CONN = sqlite3.connect('data.db')
 CUR = CONN.cursor()
@@ -15,27 +16,42 @@ API = tweepy.API(AUTH)
 
 CTA_ID = API.get_user('CTA').id
 
-def startup():
+def init():
 	'''
 	If bot just went online, do an initial scan of @CTA's 20 most recent tweets.
 	'''
 
 	# get 20 most recent @CTA tweets
-	cta_tweets = API.user_timeline(CTA_ID)
+	cta_tweets = API.user_timeline(id=CTA_ID)
 
-	for tweet in cta_tweets:
+	last_tweet_id = check_if_red_line(cta_tweets)
+	main_loop(last_tweet_id)
+
+def main_loop(last_tweet_id):
+	'''
+	Main loop that uses the most recent discovered tweet as epoch for future scans.
+	'''
+
+	while(True):
+		print('Sleeping ... \n')
+		time.sleep(60)
+		cta_tweets_since_last = API.user_timeline(id=CTA_ID, since_id=last_tweet_id)
+		
+		if cta_tweets_since_last:
+			check_if_red_line(cta_tweets_since_last)
+
+def check_if_red_line(tweet_list):
+	for tweet in tweet_list:
 		tweet_text = tweet.text.lower()
 		if 'red line' in tweet_text and 'delays' in tweet_text:
 			# retweet the Red Line alert we found
 			print('Retweeting ' + str(tweet.id) + '\n')
-		
-	# save the tweet.id of the most recent tweet we discovered
-	CUR.execute("INSERT INTO Data (Last_Tweet) VALUES (?)", (cta_tweets[0].id, ))
+
+	# save most recent tweet we found to db
+	last_tweet_id = tweet_list[0].id
+	CUR.execute("INSERT INTO Data (Last_Tweet) VALUES (?)", (last_tweet_id, ))
 	CONN.commit()
 
-	main_loop()
+	return last_tweet_id
 
-def main_loop():
-	cta_tweets_since_last = API.user_timeline
-
-startup()
+init()
