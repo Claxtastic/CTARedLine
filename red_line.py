01bc3faa6a910555eb91b5f6a981429f48a87da4
claxtastic <claxtons4@gmail.com>
2019-07-18 15:43:40 -0500

    Added logging

--- red_line.py
+++ red_line.py
@@ -3,13 +3,14 @@ import sqlite3
 import time
 import datetime
 import threading
+import logging
 
 class RedLine:
 	def __init__(self):
 		'''
 		Connect to sqlite db and create our API object
 		'''
-
+		
 		self.CONN = sqlite3.connect('data.db')
 		self.CUR = self.CONN.cursor()
 
@@ -25,7 +26,6 @@ class RedLine:
 		self.CTA_ID = self.API.get_user('CTA').id
 
 		self.last_tweet_id = self.CUR.execute('SELECT Last_Tweet FROM Data').fetchone()[0]
-		self.scan_for_tweets()
 
 	def scan_for_tweets(self):
 		'''
@@ -33,13 +33,11 @@ class RedLine:
 		'''
 
 		if self.last_tweet_id == 0:
-			# get 20 most recent @CTA tweets as a starting point
-			print('Getting starting point')
+			logging.info('Getting 20 most recent tweets as starting point.')
 			most_recent_twenty = self.API.user_timeline(id=self.CTA_ID)
 
-			# update the most recent tweet ID instance variable
+			# get the absolute most recent status ID
 			self.last_tweet_id = most_recent_twenty[0].id
-
 			self.check_if_red_line(most_recent_twenty)
 			# push most recent tweet ID to db in case we lose connection
 			self.CUR.execute('UPDATE Data SET Last_Tweet = ?', (self.last_tweet_id, )); self.CONN.commit()
@@ -51,11 +49,10 @@ class RedLine:
 				self.last_tweet_id = cta_tweets_since_last[0].id
 				self.check_if_red_line(cta_tweets_since_last)
 
-				# save most recent tweet we found to db
 				self.CUR.execute("UPDATE Data SET Last_Tweet = ?", (self.last_tweet_id, )); self.CONN.commit()
 
-			print(self.last_tweet_id)
-			print('Sleeping ... \n')
+			logging.debug('Most recent status: %s', self.last_tweet_id)
+			logging.debug('Sleeping ... ')
 			time.sleep(60)
 
 	def check_if_red_line(self, tweet_list: list):
@@ -70,46 +67,57 @@ class RedLine:
 			# we don't want replies to be processed
 			if tweet.in_reply_to_status_id is None:
 				tweet_text = tweet.text.lower()
-				print(tweet_text)
-				input()
-				# split the tweet on white space to match whole words
 				tweet_words = tweet_text.split()
 				if 'red' in tweet_words and '[' in tweet_text:
-					# retweet the Red Line alert we found
+					logging.info('Retweeting status %s.', tweet.id)
 					print('Retweeting ' + str(tweet.id) + '\n')
 					try:
 						self.API.retweet(tweet.id)
 						self._increment_incident_tally()
 					except tweepy.error.TweepError as e:
-						print('Error retweeting: ' + str(e))
+						logging.error('ERROR RETWEETING: %s', str(e))
 
 	def _increment_incident_tally(self):
 		'''
 		Increment the Incidents_Per_Month tally stored in DB.
 		'''
 
-		self.CUR.execute('UPDATE Data SET Incidents_Per_Month = Incidents_Per_Month + 1')
+		logging.debug('Incrementing Incidents_This_Month ...')
+		self.CUR.execute('UPDATE Data SET Incidents_This_Month = Incidents_This_Month + 1')
 
-def check_day():
-	'''
-	Check if it is the 1st of the month every 24 hours.
-	'''
+class CheckDay(RedLine, threading.Thread):
+	def __init__(self):
+		'''
+		Initiliaze RedLine to get our API object and thread
+		'''
 
-	while True:
-		now = datetime.datetime.now()
-		if now.day == 1:
-			# tweet the tally tweet
-			print('Tweeting tally ... ')
-			'''
-			this_minus_last = [Incidents_This_Month] - [Incidents_Last_Month]
-			if this_minus_last > 0:
-				api.tweet('There were [Incidents_This_Month]. That's up [this_minus_last] from last month.)
-			else if this_minus_last < 0:
-				api.tweet('There were [Incidents_This_Month]. That's down [abs(this_minus_last)] from last month.)
-			'''
-		time.sleep(84600)
+		RedLine.__init__(self)
+		threading.Thread.__init__(self)
 
-day_check_thread = threading.Thread(target=check_day)
-day_check_thread.start()
+	def run(self):
+		'''
+		Check if it is the 1st of the month every 24 hours.
+		'''
+
+		while True:
+			logging.debug("Checking if it's the 1st of the month ... ")
+			now = datetime.datetime.now()
+			if now.day == 1:
+				logging.info('Tweeting the monthly tally ...')
+				with sqlite3.connect('data.db') as conn:
+					cur = conn.cursor()
+					incidents_this_month, incidents_last_month = cur.execute('SELECT Incidents_This_Month, Incidents_Last_Month FROM Data').fetchone()
+					this_minus_last = incidents_this_month - incidents_last_month
+					if this_minus_last > 0:
+						self.API.update_status("There were {0} incidents in {1}. That's up {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(this_minus_last)))
+					elif this_minus_last < 0:
+						self.API.update_status("There were {0} incidents in {1}. That's down {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(this_minus_last)))
+			time.sleep(84600)
+
+logging.basicConfig(filename='log.log', format='[%(levelname)s] %(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
+check_day = CheckDay()
+check_day.start()
 
 red = RedLine()
+red.scan_for_tweets()
