de09e0733b1d10b0075263a21d8255bcd1811904
Thomas Claxton <claxtons4@gmail.com>
2020-07-15 22:07:11 -0500

    Seperate local and heroku config

--- red_line.py
+++ red_line.py
@@ -1,22 +1,61 @@
 import os
 import tweepy
-import sqlite3
 import time
 import datetime
 import threading
 import logging
 import logging.handlers as handlers
 
-class RedLine:
-	def __init__(self):
-		'''
-		Connect to sqlite db and create our API object
-		'''
-		
-		self.CONN = sqlite3.connect('data.db')
-		self.CUR = self.CONN.cursor()
+def connect_postgres():
+	'''
+	Connect to postgres and get secrets
 
-		self.check_and_set_environment()
+	Return: tuple of args to be used to start the bot
+	'''
+	import psycopg2
+
+	DATABASE_URL = os.environ['DATABASE_URL']
+	CONN = psycopg2.connect(DATABASE_URL)
+	CUR = CONN.cursor()
+
+	CUR.execute('CREATE TABLE Data (Last_Tweet INTEGER, Incidents_This_Month INTEGER, Incidents_Last_Month INTEGER)')
+
+	CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+	CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+	ACCESS_KEY = os.environ.get('ACCESS_KEY')
+	ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+
+	return (CONN, CUR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+
+def connect_sqlite():
+	'''
+	Connect to sqlite and get secrets
+
+	Return: tuple of args to be used to start the bot
+	'''
+	import sqlite3
+
+	CONN = sqlite3.connect('data.db')
+	CUR = CONN.cursor()
+
+	CONSUMER_KEY = CUR.execute('SELECT Consumer_Key FROM OAuth').fetchone()[0]
+	CONSUMER_SECRET = CUR.execute('SELECT Consumer_Secret FROM OAuth').fetchone()[0]
+	ACCESS_KEY = CUR.execute('SELECT Access_Key FROM OAuth').fetchone()[0]
+	ACCESS_SECRET = CUR.execute('SELECT Access_Secret FROM OAuth').fetchone()[0]
+
+	return (CONN, CUR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+
+class RedLine:
+	def __init__(self, CONN, CUR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET):
+		'''
+		Create our API object
+		'''
+		self.CONN = CONN
+		self.CUR = CUR
+		self.CONSUMER_KEY = CONSUMER_KEY
+		self.CONSUMER_SECRET = CONSUMER_SECRET
+		self.ACCESS_KEY = ACCESS_KEY
+		self.ACCESS_SECRET = ACCESS_SECRET
 
 		self.AUTH = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
 		self.AUTH.set_access_token(self.ACCESS_KEY, self.ACCESS_SECRET)
@@ -25,24 +64,6 @@ class RedLine:
 		self.CTA_ID = self.API.get_user('CTA').id
 
 		self.last_tweet_id = self.CUR.execute('SELECT Last_Tweet FROM Data').fetchone()[0]
-
-	def check_and_set_environment(self):
-		if os.environ.get('CONSUMER_KEY') is not None:
-			try:
-				self.CUR.execute('CREATE TABLE Data (Last_Tweet NUMBER, Incidents_This_Month NUMBER, Incidents_Last_Month NUMBER)')
-				self.CUR.execute('CREATE TABLE OAuth (Consumer_Key TEXT, Consumer_Secret TEXT, Access_Key TEXT, Access_Secret TEXT)')
-			except sqlite3.OperationalError:
-				pass
-			# on heroku
-			self.CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-			self.CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-			self.ACCESS_KEY = os.environ.get('ACCESS_KEY')
-			self.ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
-		else:
-			self.CONSUMER_KEY = self.CUR.execute('SELECT Consumer_Key FROM OAuth').fetchone()[0]
-			self.CONSUMER_SECRET = self.CUR.execute('SELECT Consumer_Secret FROM OAuth').fetchone()[0]
-			self.ACCESS_KEY = self.CUR.execute('SELECT Access_Key FROM OAuth').fetchone()[0]
-			self.ACCESS_SECRET = self.CUR.execute('SELECT Access_Secret FROM OAuth').fetchone()[0]
 
 	def scan_for_tweets(self):
 		'''
@@ -62,7 +83,8 @@ class RedLine:
 			self.last_tweet_id = most_recent_five[-1].id
 			self.check_if_red_line(most_recent_five)
 			# push most recent tweet ID to db in case we lose connection
-			self.CUR.execute('UPDATE Data SET Last_Tweet = ?', (self.last_tweet_id, )); self.CONN.commit()
+			self.CUR.execute('UPDATE Data SET Last_Tweet = ?', (self.last_tweet_id, ))
+			self.CONN.commit()
 
 		while True:
 			cta_tweets_since_last = self.API.user_timeline(id=self.CTA_ID, count=10, since_id=self.last_tweet_id)
@@ -71,7 +93,8 @@ class RedLine:
 				self.last_tweet_id = cta_tweets_since_last[-1].id
 				self.check_if_red_line(cta_tweets_since_last)
 
-				self.CUR.execute("UPDATE Data SET Last_Tweet = ?", (self.last_tweet_id, )); self.CONN.commit()
+				self.CUR.execute("UPDATE Data SET Last_Tweet = {0}".format(self.last_tweet_id))
+				self.CONN.commit()
 
 			logging.debug('Most recent status: %s', self.last_tweet_id)
 			logging.debug('Sleeping ... ')
@@ -107,13 +130,11 @@ class RedLine:
 		logging.debug('Incrementing Incidents_This_Month ...')
 		self.CUR.execute('UPDATE Data SET Incidents_This_Month = Incidents_This_Month + 1')
 
-class CheckDay(RedLine, threading.Thread):
-	def __init__(self):
-		'''
-		Initialize RedLine to get our API object and thread
-		'''
-
-		RedLine.__init__(self)
+class CheckDay(threading.Thread):
+	def __init__(self, red, CONN, CUR):
+		self.RED = red
+		self.CONN = CONN
+		self.CUR = CUR
 		threading.Thread.__init__(self)
 
 	def run(self):
@@ -126,22 +147,34 @@ class CheckDay(RedLine, threading.Thread):
 			now = datetime.datetime.now()
 			if now.day == 1:
 				logging.info('Tweeting the monthly tally ...')
-				with sqlite3.connect('data.db') as conn:
-					cur = conn.cursor()
-					incidents_this_month, incidents_last_month = cur.execute('SELECT Incidents_This_Month, Incidents_Last_Month FROM Data').fetchone()
-					delta = incidents_this_month - incidents_last_month
-					if delta > 0:
-						self.API.update_status("There were {0} incidents in {1}. That's up {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(delta)))
-					elif delta < 0:
-						self.API.update_status("There were {0} incidents in {1}. That's down {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(delta)))
-					
-					# set this month's incident total to be last month's
-					cur.execute('INSERT INTO Data Incidents_Last_Month = {0}'.format(incidents_this_month))
-					# reset counter for this month
-					cur.execute('UPDATE Data SET Incidents_This_Month = 0')
+
+				incidents_this_month, incidents_last_month = self.CUR.execute('SELECT Incidents_This_Month, Incidents_Last_Month FROM Data').fetchone()
+				delta = incidents_this_month - incidents_last_month
+				if delta > 0:
+					self.RED.API.update_status("There were {0} incidents in {1}. That's up {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(delta)))
+				elif delta < 0:
+					self.RED.API.update_status("There were {0} incidents in {1}. That's down {2} from last month.".format(incidents_this_month, now.strftime("%B"), abs(delta)))
+				
+				# set this month's incident total to be last month's
+				self.CUR.execute('INSERT INTO Data Incidents_Last_Month = {0}'.format(incidents_this_month))
+				# reset counter for this month
+				self.CUR.execute('UPDATE Data SET Incidents_This_Month = 0')
+				self.CONN.commit()
+
 			time.sleep(84600)
 
-###### Setup Logger ######
+def check_environment():
+	'''
+	Return string representing whether we are running on cloud or local
+	'''
+	if os.environ.get('DATABASE_URL') is not None:
+	# on heroku
+		return 'CLOUD'
+	else:
+		return 'LOCAL'
+
+################ Setup Logger ################
+
 log_name = 'log/log.log'
 logger = logging.getLogger('')
 logger.setLevel(logging.INFO)
@@ -154,14 +187,28 @@ log_handler.suffix = "%m%d%Y"
 log_handler.setFormatter(formatter)
 
 logger.addHandler(log_handler)
-##########################
 
-######### Start ##########
+#############################################
+
+#### Check environment and connect to DB ####
+
+environment = check_environment()
+
+if environment == 'CLOUD':
+	args = connect_postgres()
+elif environment == 'LOCAL':
+	args = connect_sqlite()
+
+################### Start ###################
+
+(CONN, CUR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET) = args
+red = RedLine(CONN, CUR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+
 # thread to check the day
-check_day = CheckDay()
+check_day = CheckDay(red, CONN, CUR)
 check_day.start()
 
-red = RedLine()
 # start loop
 red.scan_for_tweets()
-##########################
+
+#############################################
